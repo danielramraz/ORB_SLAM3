c2b2d03c8a08e9ef6bc4532ce93b99839c53c387
MichalMarom <mimarom99@gmail.com>
2023-06-11 19:21:19 +0300

    finish polyAbs

--- src/GeometricTools.cc
+++ src/GeometricTools.cc
@@ -19,6 +19,9 @@
 #include "GeometricTools.h"
 
 #include "KeyFrame.h"
+#include "PolyBase.h"
+#include "PolyAbs.h"
+#include "../../opencv/include/opencv2/opencv.hpp"
 
 namespace ORB_SLAM3
 {
@@ -44,8 +47,14 @@ Eigen::Matrix3f GeometricTools::ComputeF12(KeyFrame* &pKF1, KeyFrame* &pKF2)
     return K1.transpose().inverse() * tc1c2x * Rc1c2 * K2.inverse();
 }
 
-bool GeometricTools::Triangulate(Eigen::Vector3f &x_c1, Eigen::Vector3f &x_c2,Eigen::Matrix<float,3,4> &Tc1w ,Eigen::Matrix<float,3,4> &Tc2w , Eigen::Vector3f &x3D)
+bool GeometricTools::Triangulate(Eigen::Vector3f &x_c1, 
+                                Eigen::Vector3f &x_c2,
+                                Eigen::Matrix<float,3,4> &Tc1w ,
+                                Eigen::Matrix<float,3,4> &Tc2w , 
+                                Eigen::Vector3f &x3D)
 {
+    //---------------- linear triangulation - orb slam 3 origin ----------------------
+    /*
     Eigen::Matrix4f A;
     A.block<1,4>(0,0) = x_c1(0) * Tc1w.block<1,4>(2,0) - Tc1w.block<1,4>(0,0);
     A.block<1,4>(1,0) = x_c1(1) * Tc1w.block<1,4>(2,0) - Tc1w.block<1,4>(1,0);
@@ -63,6 +72,55 @@ bool GeometricTools::Triangulate(Eigen::Vector3f &x_c1, Eigen::Vector3f &x_c2,Ei
     x3D = x3Dh.head(3)/x3Dh(3);
 
     return true;
+    */
+
+
+    //---------------- poly abs triangulation - orb slam 3 michal and daniel ----------------------
+     /*   
+    std::vector<double> params;
+    params = {x_c1(0), x_c1(1), x_c1(2), x_c2(0), x_c2(1), x_c2(2)}
+    result = Triangulation::PolyAbs::PreparePolyCoeffs(params);
+    
+    std::cout << result << std::endl;
+
+    if (result == 0 )
+    {
+        return false;
+    }
+    else
+    {
+        x3D = result;
+        return true;
+    }
+    */
+
+    //---------------- poly abs triangulation - orb slam 3 ----------------------
+    cv::Mat P0 = cv::Mat::eye(3,4, CV_64F);
+    cv::Mat P1 = cv::Mat::eye(3,4, CV_64F);
+
+    P0 = [ Tc1w(0,0), Tc1w(0,1), Tc1w(0,2), Tc1w(0,3),
+           Tc1w(1,0), Tc1w(1,1), Tc1w(1,2), Tc1w(1,3),
+           Tc1w(2,0), Tc1w(2,1), Tc1w(2,2), Tc1w(2,3)];
+    
+    P1 = [ Tc2w(0,0), Tc2w(0,1), Tc2w(0,2), Tc2w(0,3),
+           Tc2w(1,0), Tc2w(1,1), Tc2w(1,2), Tc2w(1,3),
+           Tc2w(2,0), Tc2w(2,1), Tc2w(2,2), Tc2w(2,3)];
+
+    //cv::eigen2cv(Tc1w, &P0);
+    //cv::eigen2cv(Tc2w, &P1);
+
+    cv::Point2d point1 = cv::Point2d(x_c1(0), x_c1(1));
+    cv::Point2d point2 = cv::Point2d(x_c2(0), x_c2(1));
+
+    cv::Point3d result = Triangulation::PolyAbs(P0, P1).triangulate(point1, point2);
+    Eigen::Vector3f result_eigen = Eigen::Vector3f(static_cast<float>(result.x),
+                                                   static_cast<float>(result.y),
+                                                   static_cast<float>(result.z));
+
+    x3D = result_eigen;
+    //std::cout << result << std::endl;
+    return true;
+
 }
 
 } //namespace ORB_SLAM
